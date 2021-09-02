db95ca4e9a640d30641636cb5bd6c98e2a533727
MuteBard <csevere@brandeis.edu>
2021-09-02 10:09:59 -0400

    DEV-28: get music to not reset upon switching scenes

--- LaserDefender/Assets/Scripts/Music.cs
+++ LaserDefender/Assets/Scripts/Music.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class Music : MonoBehaviour
 {
     void Awake(){
-        //find all game objects with this tag
-        GameObject[] obj = GameObject.FindGameObjectsWithTag("music");
-        //check the number of game objects with this tag to see if it is over 1
-        if(obj.Length > 1)
-            //if so then destroy this particular gameobject that this script is on
-            Destroy(this.gameObject);
-        //if not then stay alive
-        DontDestroyOnLoad(this.gameObject);
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton(){
+       if(FindObjectsOfType(GetType()).Length > 1) {
+           gameObject.SetActive(false);
+           Destroy(gameObject);
+       }else{
+           DontDestroyOnLoad(gameObject);
+       }
     }
 }
---
+++ Notes/dev/DEV-28.md
@@ -0,0 +1,13 @@
+## DEV-28, Music player with singleton
+#### Tags: [music]
+
+
+![](../images/DEV-28-A.png)
+
+free music incase you need https://opengameart.org/content/nes-shooter-music-5-tracks-3-jingles
+
+putting music prefab at rot level fix the the annoying music resetting nonsense
+
+in the end you can export the singlton functionality but we can do that later
+
+![](../images/DEV-28-B.png)
